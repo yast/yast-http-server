438bfdd9a2cbbddcc1ea90fc8255a19ef0240beb
Uwe Gansert <ug@suse.de>
2004-02-20 15:41:35 +0000

    - more API implemented
    svn path=/trunk/http-server/; revision=14504

--- src/HTTPD.pm
+++ src/HTTPD.pm
@@ -80,7 +80,7 @@ sub GetHostList {
     } else {
         return SetError( summary => 'SCR Agent parsing failed' );
     }
-    return \@ret;
+    return @ret;
 }
 
 #map GetHost( string hostid );
@@ -123,7 +123,7 @@ sub GetHost {
             } elsif( $sslEngine eq 'on' ) {
                 $sslHash->{'VALUE'} = 1;
             }
-            return [ @{$hostHash->{'DATA'}}, $sslHash, $vbnHash ];
+            return ( @{$hostHash->{'DATA'}}, $sslHash, $vbnHash );
         }
     }
     return SetError( summary => 'hostid not found' );
@@ -257,23 +257,26 @@ sub writeHost {
 #######################################################
 
 # list<string> GetModuleList()
+BEGIN { $TYPEINFO{GetModuleList} = ["function", [ "list", "string" ] ]; }
 sub GetModuleList {
     my $data = SCR::Read('.sysconfig.apache2.APACHE_MODULES'); # FIXME: Error handling
     $data =~ s/mod_//g;
 
-    return [ split(/\s+/, $data) ];
+    return split(/\s+/, $data);
 }
 
 # list<map> GetKnownModules()
+BEGIN { $TYPEINFO{GetKnownModules} = ["function", [ "list", ["map","string","any"] ] ]; }
 sub GetKnownModules {
     my @ret = ();
     foreach my $mod ( keys(%HTTPDModules::modules) ) {
         push( @ret, { name => $mod, %{$HTTPDModules::modules{$mod}} } );
     }
-    return \@ret;
+    return @ret;
 }
 
 # bool ModifyModuleList( list<string>, bool )
+BEGIN { $TYPEINFO{ModifyModuleList} = ["function", "boolean", [ "list","string" ], "boolean" ]; }
 sub ModifyModuleList {
     my $list = shift;
 
@@ -282,54 +285,36 @@ sub ModifyModuleList {
 }
 
 # map GetKnownModulSelections()
+BEGIN { $TYPEINFO{GetKnownModulSelections} = ["function", [ "map","string","any" ] ]; }
 sub GetKnownModulSelections {
     my @ret = ();
     foreach my $sel ( keys(%HTTPDModules::selection) ) {
         push( @ret, { id => $sel, %{$HTTPDModules::selection{$sel}} } );
     }
-    return \@ret;
+    return @ret;
 }
 
 # list<string> GetModuleSelectionsList()
+BEGIN { $TYPEINFO{GetModuleSelectionsList} = ["function", ["list","string"] ]; }
 sub GetModuleSelectionsList {
-    my $activeModules = GetModuleList();
-    my $knowSelections = GetKnownModulSelections();
-    my @ret;
-
-    foreach my $selection ( @$knowSelections ) {
-        my $active = 1;
-        foreach my $selMod ( @{$selection->{'modules'}} ) {
-            unless( grep( { $selMod eq $_ } @$activeModules ) ) {
-                $active = 0;
-                last;
-            }
-        }
-        push( @ret, $selection->{id} ) if( $active );
-    }
-    return \@ret;
+    return @{(SCR::Read('.http_server.moduleselection'))[0]};
 }
 
 # bool ModifyModuleSelectionList( list<string>, bool )
+BEGIN { $TYPEINFO{ModifyModuleSelectionList} = ["function", "boolean", ["list","string"], "boolean" ]; }
 sub ModifyModuleSelectionList {
-    my $list = shift;
+    my $newSelection = shift;
     my $enable = shift;
-    my $activeMods = GetModuleList();
-    my %modCounter = ();
+    my %uniq = ();
 
-    foreach my $mod ( @$activeMods ) {
-        $modCounter{$mod} = 1;
+    @uniq{GetModuleSelectionsList()} = ();
+    if( $enable ) {
+        @uniq{@$newSelection} = ();
+    } else {
+        delete(@uniq{@$newSelection});
     }
 
-    foreach my $workSel ( @$list ) {
-        if( $enable ) {
-            foreach my $mod ( @{$HTTPDModules::selection{$workSel}->{modules}} ) {
-                $modCounter{$mod} = 1;
-            }
-        } else {
-
-        }
-    }
-    ModifyModuleList( [ keys(%modCounter) ] );
+    SCR::Write('.http_server.moduleselection', [keys(%uniq)]);
 }
 
 #######################################################
@@ -343,6 +328,7 @@ sub ModifyModuleSelectionList {
 #######################################################
 
 # boolean ModiflyService( boolean )
+BEGIN { $TYPEINFO{ModifyService} = ["function", "boolean", "boolean" ]; }
 sub ModifyService {
     my $enable = shift;
 
@@ -366,27 +352,29 @@ sub ModifyService {
 # apache2 listen ports
 #######################################################
 # boolean CreateListen( int, int, list<string> )
+BEGIN { $TYPEINFO{CreateListen} = ["function", "boolean", ["int", "int", [ "list", "string" ] ] ]; }
 sub CreateListen {
     my $fromPort = shift;
     my $toPort = shift;
-    my $ip = shift;
+    my $ip = shift; #FIXME: this is a list
 
-    my $listenEntries = GetCurrentListen();
+    my @listenEntries = GetCurrentListen();
     my %newEntry;
     $newEntry{ADDRESS} = $ip if ($ip);
     $newEntry{PORT} = ($fromPort eq $toPort)?($fromPort):($fromPort.'-'.$toPort);
-    SCR::Write( ".http_server.listen", [ @$listenEntries, \%newEntry ] );
+    SCR::Write( ".http_server.listen", [ @listenEntries, \%newEntry ] );
 }
 
 # boolean CreateListen( int, int, list<string> )
+BEGIN { $TYPEINFO{DeleteListen} = ["function", "boolean", ["int", "int", [ "list", "string" ] ] ]; }
 sub DeleteListen {
     my $fromPort = shift;
     my $toPort = shift;
-    my $ip = shift;
+    my $ip = shift; #FIXME: this is a list
 
-    my $listenEntries = GetCurrentListen();
+    my @listenEntries = GetCurrentListen();
     my @newListenEntries = ();
-    foreach my $listen ( @$listenEntries ) {
+    foreach my $listen ( @listenEntries ) {
         if( defined($ip) and (not exists($listen->{'ADDRESS'}) or $listen->{'ADDRESS'} ne $ip) ) {
             push( @newListenEntries, $listen );
             next;
@@ -400,6 +388,7 @@ sub DeleteListen {
 }
 
 # list<map> GetCurrentListen()
+BEGIN { $TYPEINFO{GetCurrentListen} = ["function", ["list", [ "map", "string", "any" ] ] ]; }
 sub GetCurrentListen {
     my @data = SCR::Read('.http_server.listen');
     my @ret;
@@ -410,23 +399,62 @@ sub GetCurrentListen {
             push( @ret, { PORT => $listen } );
         }
     }
-    return \@ret;
+    return @ret;
 }
 
 #######################################################
 # apache2 listen ports end
 #######################################################
 
+
+
+#######################################################
+# apache2 pacakges
+#######################################################
+
+# list<string> GetServicePackages();
+BEGIN { $TYPEINFO{GetServicePackages} = ["function", ["list", [ "map", "string", "any" ] ] ]; }
+sub GetServicePackages {
+    return 'apache2'; #???
+}
+
+# list<string> GetModulePackages
+BEGIN { $TYPEINFO{GetModulePackages} = ["function", ["list", "string"] ]; }
+sub GetModulePackages {
+    my $mods = GetModuleSelectionsList();
+    my %uniq;
+
+    foreach my $mod ( @$mods ) {
+        @uniq{@{$mod->{packages}}} = ();
+    }
+    return keys(%uniq);
+}
+
+#######################################################
+# apache2 packages end
+#######################################################
+
+
+#######################################################
+# apache2 firewall
+#######################################################
+
+#######################################################
+# apache2 firewall end
+#######################################################
+
+
+
 sub run {
     print "-------------- GetHostList\n";
-    foreach my $h ( @{GetHostList()} ) {
+    foreach my $h ( GetHostList() ) {
         print "ID: $h\n";
     }
 
     print "-------------- ModifyHost Number 0\n";
     my $hostid = "default";
-    my $hostArr = GetHost( $hostid );
-    ModifyHost( $hostid, $hostArr );
+    my @hostArr = GetHost( $hostid );
+    ModifyHost( $hostid, \@hostArr );
 
     print "-------------- CreateHost\n";
     my @temp = (
@@ -437,9 +465,9 @@ sub run {
     CreateHost( '192.168.1.2/createTest2.suse.de', \@temp );
 
     print "-------------- GetHost created host\n";
-    $hostArr = GetHost( '192.168.1.2/createTest2.suse.de' );
+    @hostArr = GetHost( '192.168.1.2/createTest2.suse.de' );
     use Data::Dumper;
-    print Data::Dumper->Dump( [ $hostArr ] );
+    print Data::Dumper->Dump( [ \@hostArr ] );
 
     system("cat /etc/apache2/vhosts.d/yast2_vhosts.conf");
 
@@ -447,26 +475,28 @@ sub run {
     DeleteHost( '192.168.1.2/createTest2.suse.de' );
 
     print "-------------- show module list\n";
-    foreach my $mod ( @{GetModuleList()} ) {
+    foreach my $mod ( GetModuleList() ) {
         print "MOD: $mod\n";
     }
 
     print "-------------- show known modules\n";
-    foreach my $mod ( @{GetKnownModules()} ) {
+    foreach my $mod ( GetKnownModules() ) {
         print "KNOWN MOD: $mod->{name}\n";
     }
 
-    print "-------------- show known modules\n";
-    foreach my $mod ( @{GetKnownModulSelections()} ) {
+    print "-------------- show known selections\n";
+    foreach my $mod ( GetKnownModulSelections() ) {
         print "KNOWN SEL: $mod->{id}\n";
     }
+
+    print "-------------- show active selections\n";
+    GetModuleSelectionsList();
 
     print "-------------- activate apache2\n";
     ModifyService(1);
 
     print "-------------- get listen\n";
-    my $listen = GetCurrentListen();
-    foreach my $l ( @$listen ) {
+    foreach my $l ( GetCurrentListen() ) {
         print "$l->{ADDRESS}:" if( $l->{ADDRESS} );
         print $l->{PORT}."\n";
     }
@@ -475,8 +505,7 @@ sub run {
     DeleteListen( 443,443 );
     DeleteListen( 80,80,"12.34.56.78" );
     print "-------------- get listen\n";
-    my $listen = GetCurrentListen();
-        foreach my $l ( @$listen ) {
+    foreach my $l ( GetCurrentListen() ) {
         print "$l->{ADDRESS}:" if( $l->{ADDRESS} );
         print $l->{PORT}."\n";
     }
@@ -485,9 +514,20 @@ sub run {
     CreateListen( 443,443 );
     CreateListen( 80,80,"12.34.56.78" );
 
+    print "--------------set ModuleSelections\n";
+    ModifyModuleSelectionList( [ 'mod_test1', 'mod_test2', 'mod_test3' ], 1 );
+    ModifyModuleSelectionList( [ 'mod_test3' ], 0 );
+
+    print "-------------- get ModuleSelections\n";
+    foreach my $sel ( GetModuleSelectionsList() ) {
+        print "SEL: $sel\n";
+    }
+
+
+
     print "--------------trigger error\n";
-    $hostid = GetHost( 'will.not.be.found' );
-    unless( $hostid ) {
+    my @host = GetHost( 'will.not.be.found' );
+    if( @host and not(defined($host[0])) ) {
         my %error = Error();
         while( my ($k,$v) = each(%error) ) {
             print "ERROR: $k = $v\n";
