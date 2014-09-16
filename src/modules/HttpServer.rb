3b485769b1e5a11706b43b6b207e0f1e8bfe0c71
Stefan Schubert <schubi@suse.de>
2014-09-16 10:13:43 +0200

    using nicer none?

--- src/modules/HttpServer.rb
+++ src/modules/HttpServer.rb
@@ -596,8 +596,7 @@ module Yast
         else
           value = row["VALUE"] || []
           key_split = row["KEY"].split("/")
-          if value.select{|item| item.has_key?("HostIP")}.empty? &&
-            key_split.size > 1
+          if value.none? {|item| item.has_key?("HostIP")} && key_split.size > 1
             # Set HostIP which is given in the KEY (e.g. *:443/sleposbuilder3.suse.cz.conf)
             # values in order to set VirtualHost in /etc/apache2/vhosts.d/<hostname>
             # (bnc#895127)
