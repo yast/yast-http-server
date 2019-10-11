cf9f21d719f646ec26f35a6e9d5bfe319f4db090
Martin Vidner <mvidner@suse.cz>
2019-10-11 16:24:37 +0200

    Smoke test

--- test/http_server_test.rb
+++ test/http_server_test.rb
@@ -3,5 +3,16 @@
 require_relative "test_helper"
 require "http-server/clients/main"
 
+Yast.import "CommandLine"
+
 describe Yast::HttpServerClient do
+  describe "#main" do
+    before do
+      allow(Yast::CommandLine).to receive(:Run)
+    end
+
+    it "does not crash" do
+      expect { subject.main }.to_not raise_error
+    end
+  end
 end
