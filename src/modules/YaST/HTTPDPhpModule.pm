c5f960467fa3e09e117b84b4015d09d3cc5c4977
Michal Filka <mfilka@suse.cz>
2022-07-26 09:50:34 +0200

    Dynamically query for available php version

--- src/modules/YaST/HTTPDPhpModule.pm
+++ src/modules/YaST/HTTPDPhpModule.pm
@@ -1,11 +1,27 @@
 package YaST::HTTPDPhpModule;
 
+use YaST::YCP;
+
+YaST::YCP::Import "Package";
+
 our %TYPEINFO;
 
 # Define globally the current PHP version
 BEGIN { $TYPEINFO{Version} = ["function", "string" ]; }
 sub Version {
-    return "7";
+    # when function returns an array, we get reference to it
+    $l = Package->by_pattern("php[0-9]");
+
+    return "" if(!$l);
+
+    # there can be multiple versions of php
+    @{$l} = sort @{$l};
+
+    # package name is php<version> and we're interested in <version> only
+    # we also take highest available version
+    $l->[-1] =~ s/php([0-9]{1,2})$/$1/;
+
+    return $l->[-1];
 }
 
 1;
