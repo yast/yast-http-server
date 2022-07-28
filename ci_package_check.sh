f5d575814fe5da8779906b7443c68c469f54dd20
Ladislav Slezák <lslezak@suse.cz>
2022-07-28 14:21:42 +0200

    CI fix

--- ci_package_check.sh
+++ ci_package_check.sh
@@ -3,4 +3,7 @@
 set -e
 make -f Makefile.cvs
 make install
+# refresh the repositories from script, YaST wants to display
+# a progress which does not work properly in GitHub Actions
+zypper ref
 ./ci_package_check.rb
